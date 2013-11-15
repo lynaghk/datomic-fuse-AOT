d5bc596c848e185bf0cf417a64e4024a09c5fc2e
Kevin J. Lynagh <kevin@keminglabs.com>
2013-11-15 15:24:09 -0800

    Ah ha! full text search in fuse is the problem. I wonder if just full text search in a proxy would do it...

--- src/duber/main.clj
+++ src/duber/main.clj
@@ -1,40 +1,57 @@
 (ns duber.main
   (:require [datomic.api :as d])
+    (:import java.io.File
+           (net.fusejna DirectoryFiller
+                        ErrorCodes
+                        FuseFilesystem
+                        StructFuseFileInfo$FileInfoWrapper
+                        StructStat$StatWrapper)
+           net.fusejna.types.TypeMode$NodeType
+           net.fusejna.types.TypeMode$ModeWrapper
+           net.fusejna.util.FuseFilesystemAdapterFull)
   (:gen-class))
 
-(definterface IFoo
-  (^int foo [x]))
+(defn mkfs [db]
+    (proxy [FuseFilesystemAdapterFull] []
+
+      (getattr
+        [^String path ^StructStat$StatWrapper stat]
+        (prn "getattr" path)
+        (.setMode stat TypeMode$NodeType/DIRECTORY)
+        0)
+      
+      (readdir
+        [^String path ^DirectoryFiller filler]
+        (prn "reading directory")
+        (let [^Iterable entries (->> (d/q '[:find ?n :where [(fulltext $ :duber/name "hello") [[?e ?n]]]]
+                                          db)
+                                     first)]
+          (.add filler entries))
+        0)))
 
 (defn -main [& args]
 
   (let [uri (str "datomic:mem://" (gensym))
         _ (d/create-database uri)
-        conn (d/connect uri)
-        f (proxy [IFoo] []
-            (foo [x]
+        conn (d/connect uri)]
+    
+        (println @(d/transact conn
+                              [{:db/doc "A name"
+                                :db/ident :duber/name
+                                :db/valueType :db.type/string
+                                :db/cardinality :db.cardinality/one
+                                :db/fulltext true
+                                :db/id #db/id[:db.part/db]
+                                :db.install/_attribute :db.part/db}]))
 
-              (println "running foo")
+        (println @(d/transact conn
+                              [{:db/id #db/id[:db.part/user]
+                                :duber/name "hello world"}]))
 
-              (println @(d/transact conn
-                                    [{:db/doc "A name"
-                                      :db/ident :duber/name
-                                      :db/valueType :db.type/string
-                                      :db/cardinality :db.cardinality/one
-                                      :db/fulltext true
-                                      :db/id #db/id[:db.part/db]
-                                      :db.install/_attribute :db.part/db}]))
+        (.mount (mkfs (d/db conn))
+                (doto (java.io.File. "foo/")
+                  (.mkdirs))
+                false))
 
-              (println @(d/transact conn
-                                    [{:db/id #db/id[:db.part/user]
-                                      :duber/name "hello world"}]))
-
-              (println (d/q '[:find ?n :where [?e :duber/name ?n]]
-                            (d/db conn)))
-
-
-              1))]
-
-      (.run (Thread. (fn []
-                       (println "in a thread")
-                       (.foo f "hmm."))))
-    (System/exit 0)))
+  ;;(System/exit 0)
+  )
